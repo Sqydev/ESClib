d1ce3663cf9047a8f2b4bf97379801a05f2e03e6
Sqyd <wojkap10@proton.me>
2025-10-21 20:05:59 +0200

    Cursor things

--- include/tuiutils.h
+++ include/tuiutils.h
@@ -11,6 +11,12 @@ void ClearChar();
 
 void SwitchAlternateBuffMode();
 
+void SetCursorPos(int x, int y);
+
+void MoveCursorDirectional(int up, int down, int left, int right);
+
+void MoveCursor(int x, int y);
+
 
 
 #endif
--- src/TuiUtils.c
+++ src/TuiUtils.c
@@ -35,3 +35,24 @@ void SwitchAlternateBuffMode() {
 		AlternateBuffModeActive = false;
 	}
 }
+
+void SetCursorPos(int x, int y) {
+	printf("\033[%d;%dH", y, x);
+	fflush(stdout);
+}
+
+void MoveCursorDirectional(int up, int down, int left, int right) {
+	if (up > 0) printf("\033[%dA", up);
+	if (down > 0) printf("\033[%dB", down);
+	if (right > 0) printf("\033[%dC", right);
+	if (left > 0) printf("\033[%dD", left);
+	fflush(stdout);
+}
+
+void MoveCursor(int x, int y) {
+	if (y < 0) printf("\033[%dA", -y);
+	if (y > 0) printf("\033[%dB", y);
+	if (x > 0) printf("\033[%dC", x);
+	if (x < 0) printf("\033[%dD", -x);
+	fflush(stdout);
+}
