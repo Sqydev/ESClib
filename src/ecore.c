5c8d1fdccfe9e19eb0754e2bb7d53f6ac7f0bfe4
Sqyd <wojkap10@proton.me>
2025-12-16 20:52:29 +0100

    Secret

--- src/ecore.c
+++ src/ecore.c
@@ -638,6 +638,6 @@ void DisableBufferMode(void) {
 
 __asm__(
 	".pushsection .comment\n"
-	".asciz \"Made by Sqyd/_Sqyd_/Sqydev, github: https://github.com/Sqydev\"\n"
+	".asciz \" Using or just .so being here ESClib(lib for tui) with was made by Sqyd/_Sqyd_/Sqydev, github: https://github.com/Sqydev\"\n"
 	".popsection\n"
 );
