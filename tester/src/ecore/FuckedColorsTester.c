0bb9a13ecbe21e40ee38c2010c86584abdeeeece
Sqyd <138494523+Sqydev@users.noreply.github.com>
2026-06-08 21:44:22 +0200

    dk

--- tester/src/ecore/FuckedColorsTester.c
+++ tester/src/ecore/FuckedColorsTester.c
@@ -7,7 +7,7 @@ void FuckedUpColorsTest() {
 		BeginDrawing();
 		ClearTui((Color){ 0,0,0,0 }, (Color){ 255,255,255,1 });
 
-	 	DrawRectanglePro("$", 40, 55, 40, 10, 0, 0, &(Color){ 255, 0, 0, 1 }, &(Color){ 0, 0, 255, 1 }, 0, 5, false, true);
+	 	DrawRectanglePro("$", 40, 55, 40, 10, 0, 0, &(Color){ 255, 0, 0, 1 }, &(Color){ 0, 0, 255, 1 }, 0, 5, false, 0, true);
 
 		EndDrawing();
 	}
